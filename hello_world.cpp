3acdfc7723b4e9f0c17c7e0fb165b03c7abe8e3c
Ian <ian.delgado.5021@mail.linnbenton.edu>
2021-01-06 14:21:20 -0800

    first commit

---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+//first c++ program that I will push into my GitHub repository for my assignemnt 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, Mr. Jess!"<< std::endl;
+    return 0;
+}
